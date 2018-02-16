c58e2bd242339b71f70ce0cc8b0efe8805f3c346
linazoo <linazooper@gmail.com>
2018-02-16 11:27:56 -0600

    added componentDidMount to trigger the interval for checking mouse position. added propTypes for MoveObjects

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
 
 class App extends Component {
+  componentDidMount() {
+    const self = this;
+    setInterval(() => {
+      self.props.moveObjects(self.canvasMousePosition);
+    }, 10);
+  }
+
+  trackMouse(event) {
+    this.canvasMousePosition = getCanvasPosition(event);
+  }
+
   render() {
     return (
-      <Canvas />
+      <Canvas 
+        angle={ this.props.angle }
+        trackMouse={event => (this.trackMouse(event))}
+      />
     );
   }
 }
 
 App.PropTypes = {
-  message: PropTypes.string.isRequired,
+  angle: PropTypes.number.isRequired,
+  message: PropTypes.func.isRequired,
 };
 export default App;
