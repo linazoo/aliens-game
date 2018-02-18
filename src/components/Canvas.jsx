ca552fe020ec99180b5a7a84eb7cecd509bf172d
linazoo <linazooper@gmail.com>
2018-02-18 14:39:00 -0600

    conditionally showing title or flying objects based on gamestate.started

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -16,34 +16,49 @@ const Canvas = (props) => {
   const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];  
   return (
     <svg
-    id="aliens-game-canvas"
-    preserveAspectRatio="xMaxYMax none"
-    onMouseMove={ props.trackMouse }
-    viewBox={ viewBox }
+      id="aliens-go-home-canvas"
+      preserveAspectRatio="xMaxYMax none"
+      onMouseMove={props.trackMouse}
+      viewBox={viewBox}
     >
-    <defs>
-      <filter id="shadow">
-        <feDropShadow dx="1" dy="1" stdDeviation="2" />
-      </filter>
-    </defs>
-    <Sky />
-    <Ground />
-    <CannonBall position={{x: 0, y: -100}}/>
-    <CannonPipe rotation={props.angle} />
-    <CannonBase />
-    <CurrentScore score={15}/>
-    <FlyingObject position={{x: -150, y: -300}}/>
-    <FlyingObject position={{x: 150, y: -300}}/>
-    <Heart position={{x: -300, y: 35}} />
-    <StartGame onClick={() => console.log('Illegals, Go Home!')} />
-    <Title />
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
+      <Sky />
+      <Ground />
+      <CannonPipe rotation={props.angle} />
+      <CannonBase />
+      <CurrentScore score={15} />
+
+      { ! props.gameState.started &&
+        <g>
+          <StartGame onClick={() => props.startGame()} />
+          <Title />
+        </g>
+      }
+
+      { props.gameState.started &&
+        <g>
+          <FlyingObject position={{x: -150, y: -300}}/>
+          <FlyingObject position={{x: 150, y: -300}}/>
+        </g>
+      }
     </svg>
   );
 };
 
-Canvas.PropTypes = {
+Canvas.propTypes = {
   angle: PropTypes.number.isRequired,
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+  }).isRequired,
   trackMouse: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
 };
+
 
 export default Canvas;
