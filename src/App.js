7de9daa58d4ed7cbe4ef09c8082c58e01f08db1a
linazoo <linazooper@gmail.com>
2018-02-19 12:56:02 -0600

    configured auth0 to handle authentication

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
+import * as Auth0 from 'auth0-web';
+
+Auth0.configure({
+  domain: 'sevenseas.auth0.com',
+  clientID: '5hLK3VFTFMD4cYg0nF5HEoTeoWiihFx1',
+  redirectUri: 'http://localhost:3000/',
+  responseType: 'token id_token',
+  scope: 'openid profile manage:points',
+});
 
 class App extends Component {
   componentDidMount() {
+
+    Auth0.handleAuthCallback();
+    
+    Auth0.subscribe((auth) => {
+      console.log(auth);
+    });
+
     setInterval(() => {
       this.props.moveObjects(this.canvasMousePosition);
     }, 10);
