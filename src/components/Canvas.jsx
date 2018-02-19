ffcb70bb481ac38f6d80c580f1f37aab7d0c2414
linazoo <linazooper@gmail.com>
2018-02-19 13:24:34 -0600

    created a leaderboard React component

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -17,14 +17,14 @@ const Canvas = (props) => {
   const gameHeight = 1200;
   const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];  
   const leaderboard = [
-    { id: 'd4', maxScore: 82, name: 'Ado Kukic', picture: 'https://twitter.com/KukicAdo/profile_image', },
-    { id: 'a1', maxScore: 235, name: 'johnny lyden', picture: 'https://twitter.com/brunoskrebs/profile_image', },
-    { id: 'c3', maxScore: 99, name: 'Diego Poza', picture: 'https://twitter.com/diegopoza/profile_image', },
-    { id: 'b2', maxScore: 129, name: 'Jeana Tahnk', picture: 'https://twitter.com/jeanatahnk/profile_image', },
-    { id: 'e5', maxScore: 34, name: 'Jenny Obrien', picture: 'https://twitter.com/jenny_obrien/profile_image', },
-    { id: 'f6', maxScore: 153, name: 'Kim Maida', picture: 'https://twitter.com/KimMaida/profile_image', },
-    { id: 'g7', maxScore: 55, name: 'Luke Oliff', picture: 'https://twitter.com/mroliff/profile_image', },
-    { id: 'h8', maxScore: 146, name: 'Sebastián Peyrott', picture: 'https://twitter.com/speyrott/profile_image', },
+    { id: 'd4', maxScore: 82, name: 'Iggy Pop', picture: 'https://twitter.com/KukicAdo/profile_image', },
+    { id: 'a1', maxScore: 235, name: 'Ozzi Osborne', picture: 'https://twitter.com/brunoskrebs/profile_image', },
+    { id: 'c3', maxScore: 99, name: 'Kurt Cobain', picture: 'https://twitter.com/diegopoza/profile_image', },
+    { id: 'b2', maxScore: 129, name: 'Amy Winehouse', picture: 'https://twitter.com/jeanatahnk/profile_image', },
+    { id: 'e5', maxScore: 34, name: 'Michael Jordan', picture: 'https://twitter.com/jenny_obrien/profile_image', },
+    { id: 'f6', maxScore: 153, name: 'Anna Nicole Smith', picture: 'https://twitter.com/KimMaida/profile_image', },
+    { id: 'g7', maxScore: 55, name: 'Courtney Love', picture: 'https://twitter.com/mroliff/profile_image', },
+    { id: 'h8', maxScore: 146, name: 'Zombie Chick', picture: 'https://twitter.com/speyrott/profile_image', },
   ];
   
   return (
