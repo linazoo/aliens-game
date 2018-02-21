565db5e98e5486615b0a81da34d4bbf753f3b0f7
linazoo <linazooper@gmail.com>
2018-02-20 21:24:36 -0600

    connected app wih the auth0 backend service and database calling backend when users login to get the users info and set that user as currentUser in the app state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
 import * as Auth0 from 'auth0-web';
+import io from 'socket.io-client';
+
 
 Auth0.configure({
   domain: 'sevenseas.auth0.com',
@@ -10,15 +12,52 @@ Auth0.configure({
   redirectUri: 'http://localhost:3000/',
   responseType: 'token id_token',
   scope: 'openid profile manage:points',
+  audience: 'https://linazoo.github.io/aliens-game/',
 });
 
 class App extends Component {
   componentDidMount() {
 
     Auth0.handleAuthCallback();
-    
+
     Auth0.subscribe((auth) => {
-      console.log(auth);
+      if (!auth) return;
+
+      const playerProfile = Auth0.getProfile();
+      const currentPlayer = {
+        id: playerProfile.sub,
+        maxScore: 0,
+        name: playerProfile.name,
+        picture: playerProfile.picture,
+      };
+
+      this.props.loggedIn(currentPlayer);
+
+      const socket = io('http://localhost:3001', {
+        query: `token=${Auth0.getAccessToken()}`,
+      });
+
+      let emitted = false;
+      socket.on('players', (players) => {
+        this.props.leaderboardLoaded(players);
+
+        if (emitted) return;
+        socket.emit('new-max-score', {
+          id: playerProfile.sub,
+          maxScore: 120,
+          name: playerProfile.name,
+          picture: playerProfile.picture,
+        });
+        emitted = true;
+        setTimeout(() => {
+          socket.emit('new-max-score', {
+            id: playerProfile.sub,
+            maxScore: 222,
+            name: playerProfile.name,
+            picture: playerProfile.picture,
+          });
+        }, 5000);
+      });
     });
 
     setInterval(() => {
@@ -39,9 +78,11 @@ class App extends Component {
 
   render() {
     return (
-      <Canvas 
-        angle={ this.props.angle }
+      <Canvas
+        angle={this.props.angle}
+        currentPlayer={this.props.currentPlayer}
         gameState={this.props.gameState}
+        players={this.props.players}
         startGame={this.props.startGame}
         trackMouse={event => (this.trackMouse(event))}
       />
@@ -65,6 +106,25 @@ App.propTypes = {
   }).isRequired,
   moveObjects: PropTypes.func.isRequired,
   startGame: PropTypes.func.isRequired,
+  currentPlayer: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  }),
+  leaderboardLoaded: PropTypes.func.isRequired,
+  loggedIn: PropTypes.func.isRequired,
+  players: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  })),
+};
+
+App.defaultProps = {
+  currentPlayer: null,
+  players: null,
 };
 
 export default App;
