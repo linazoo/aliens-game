ab7aed6716cb0e0bbdb5cae49d61251de84f8aba
linazoo <linazooper@gmail.com>
2018-02-25 12:16:04 -0600

    added functionality to decrement lives when a flyingObject makes it to earth - game resets when all lives lost

--- src/reducers/moveObjects.js
+++ src/reducers/moveObjects.js
@@ -22,6 +22,20 @@ function moveObjects(state, action) {
     (now - object.createdAt) < 4000
   ));
 
+  const lostLife = state.gameState.flyingObjects.length > flyingObjects.length;
+  let lives = state.gameState.lives;
+  if (lostLife) {
+    lives--;
+  }
+
+  const started = lives > 0;
+  if (!started) {
+    flyingObjects = [];
+    cannonBalls = [];
+    lives = 7;
+  }
+
+
   const { x,y } = mousePosition;
   const angle = calculateAngle(0,0,x,y);
 
@@ -37,7 +51,9 @@ function moveObjects(state, action) {
     gameState: {
       ...newState.gameState,
       flyingObjects,
-      cannonBalls,
+      cannonBalls: [...cannonBalls],
+      lives,
+      started,
     },
     angle,
   };
